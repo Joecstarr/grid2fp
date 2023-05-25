4a561ba56cca2ee1eb11d3b9914733202cd0e2f6
joe.c.starr <joe.c.starr@gmail.com>
2023-05-24 23:09:19 -0500

    fix lic

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     keywords=["topology", "Legendrian", "Grid Diagram","knot"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Framework :: IPython",
     ],

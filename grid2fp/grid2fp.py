6496eb99c853651a57199c72da5006ccc1a4d60c
Joe Starr <52646388+Joecstarr@users.noreply.github.com>
2023-05-25 15:34:00 -0400

    rotation direction issue

--- grid2fp/grid2fp.py
+++ grid2fp/grid2fp.py
@@ -66,7 +66,7 @@ class grid2fp:
             rotated cord as tuple
         """
         r = math.sqrt(2) / 2
-        return ((x + y * r) * self.scale, (y - x * r) * self.scale)
+        return ((x - y * r) * self.scale, (y + x * r) * self.scale)
 
     def __get_segments(self):
         """Parse the grid for segments."""
